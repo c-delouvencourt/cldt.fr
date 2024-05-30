4f2d9896515e1e22c3077374048a3c7510863d30
c-delouvencourt <clement@meilleursbiens.com>
2024-05-30 18:41:46 +0200

    deploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,10 +9,10 @@ export const metadata: Metadata = {
   title: {
     template: '%s - Clément de Louvencourt',
     default:
-      'Clément de Louvencourt - Software designer, entrepreneur, and amateur runner',
+      'Clément de Louvencourt - Software engineer, entrepreneur, and amateur runner',
   },
   description:
-    'I’m Clément, a software designer and entrepreneur based in Paris. I’m Clément, a software designer and entrepreneur based in Paris. I’m the CTO of MeilleursBiens, where we develop technologies that that enable real estate pros to save twice as much time and money in their business.',
+    'I’m Clément, a software designer and entrepreneur based in Paris. I’m Clément, a software designer and entrepreneur based in Paris. I’m the CTO of MeilleursBiens, where we develop technologies that enable real estate pros to save twice as much time and money in their business.',
   alternates: {
     types: {
       'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
